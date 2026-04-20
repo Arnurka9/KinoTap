be9cedb1b52b634b34f56a7682538bdfe3cbeafc
Arnurka9 <arnurchikkun@gmail.com>
2026-04-20 15:57:39 +0500

    DRF config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,11 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
+    'corsheaders',
+    
     'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddeware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
 ]
 
 ROOT_URLCONF = 'config.urls'
