475e466dd32335780292d3b51310c23e49c83873
Zhanel <nursapazhan@gmail.com>
2026-04-22 10:49:26 +0500

    Add GenreSerializer for Genre model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Genre
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
