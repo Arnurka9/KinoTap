895d321080e56c18ed9fd11fcd25e3ed54d0f13d
Zhanel <nursapazhan@gmail.com>
2026-04-23 18:56:02 +0500

    Add movie routes and include router for API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views.auth_views import LogoutView 
+from rest_framework.routers import DefaultRouter
+
+from .views import register, genre_list, LogoutView
+from .views.movie_views import MovieViewSet, movie_list, movie_detail
+
+
+router = DefaultRouter()
+router.register('movies', MovieViewSet)
+
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -11,4 +19,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', register),
     path('genres/', genre_list),
+    path('movies-old/', movie_list),
+    path('movies-old/<int:pk>/', movie_detail),
+    path('', include(router.urls)),
 ]
