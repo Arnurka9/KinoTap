d38a1f0f63608fee867d31b51da28f8caf3a5330
Zhanel <nursapazhan@gmail.com>
2026-04-23 19:09:40 +0500

    Implement AuthService for user registration and login

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private api = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  register(data: any) {
+    return this.http.post(`${this.api}/register/`, data);
+  }
+
+  login(data: any) {
+    return this.http.post<any>(`${this.api}/login/`, data);
+  }
+}
