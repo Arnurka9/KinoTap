cc86dcc85c7c95a7a43d70b4fb1f07e9550de02b
Zhanel <nursapazhan@gmail.com>
2026-04-23 19:07:49 +0500

    Define application routes for movie and auth components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AddMovieComponent } from './movies/add-movie/add-movie';
+import { RegisterComponent } from './auth/register/register';
+import { LoginComponent } from './auth/login/login';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'add-movie', component: AddMovieComponent },
+
+  {
+    path: 'edit-movie/:id',
+    loadComponent: () =>
+      import('./movies/edit-movie/edit-movie').then(m => m.EditMovieComponent)
+  },
+  { 
+    path: '', redirectTo: 'add-movie', pathMatch: 'full' },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'add-movie', component: AddMovieComponent },
+  { path: 'login', component: LoginComponent }, 
+];
