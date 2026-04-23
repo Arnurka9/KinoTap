03dc10006cdece66d3ccfc35cb6aea5a28e403ec
Zhanel <nursapazhan@gmail.com>
2026-04-23 18:55:15 +0500

    Add MovieSerializer and enhance RegisterSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Genre
+from .models import Genre, Movie
+
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,14 +10,31 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'username', 'password', 'password2']
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists")
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password']
-        )
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
         return user
+    
+class MovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Movie
+
+        fields = '__all__'
