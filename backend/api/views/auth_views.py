1bcf909fc51ff314cfb650e37a37bbf314899023
Zhanel <nursapazhan@gmail.com>
2026-04-23 18:51:55 +0500

    Update auth_views.py

--- backend/api/views/auth_views.py
+++ backend/api/views/auth_views.py
@@ -1,10 +1,27 @@
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.exceptions import ValidationError
 
+from ..serializers import RegisterSerializer
+
+
+# REGISTER
+@api_view(['POST'])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# LOGOUT
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
 
